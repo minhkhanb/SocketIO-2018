4b6a38710df96f700e8480f91f3d1bde766620b7
Kha Le <minhkhanb@gmail.com>
2018-11-20 12:03:38 +0700

    init

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var app = require('express')();
+var express = require('express')
+var mongooes = require('mongoose');
+var dbUrl = 'mongodb://<admin>:<chat123>@ds113134.mlab.com:13134/single-chat'
+
+var app = express();
+
+app.use(express.static(__dirname + '/static'));
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+mongooes.connect(dbUrl, function(err) {
+  console.log('mongodb connected', err);
+});
+
+var Message = mongooes.model('Message', { name: String, message: String });
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
